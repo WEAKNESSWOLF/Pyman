28525f752c3c759034a056dc1b8122643945f635
WEAKNESS WOLF <ottojboy@gmail.com>
2022-06-24 21:04:00 +0200

    testing packages

---
+++ main.py
@@ -0,0 +1,9 @@
+import random as randint
+
+class Map:
+
+    def __init__(self):
+        self.width = randint(30, 50)
+        self.height = self.width
+
+    # def create_barears(self):
---
+++ player.py
@@ -0,0 +1,12 @@
+from random import randint
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print("Player")
